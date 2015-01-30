b7e67b8619ca1ec8acb42b2d140e874109eaf90e
Dan Fox <iamdanfox@gmail.com>
2015-01-30 22:19:10 +0000

    Back to hot module reload

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,18 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    // 'webpack-dev-server/client?http://localhost:3000',
-    // 'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/dev-server',
     './scripts/Entry.cjsx'
   ],
   output: {
     path: __dirname,
     filename: 'bundle.js',
+    publicPath: '/scripts/'
   },
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin()
-  // ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx', '.coffee', '.cjsx', '.less']
   },
