8a9bc957303973d074e1f671777600cad1b0303b
Dan Fox <iamdanfox@gmail.com>
2015-02-01 14:52:48 +0000

    graph geometry

--- scripts/Surface.jsx
+++ scripts/Surface.jsx
@@ -86,35 +86,36 @@ var Surface = React.createClass({
 //     )
 //     @scene.add( @graph )
 
-//   buildGraphGeometry: (props) ->
-//     polarMeshFunction = (i,j) ->
-//       theta = i * 2 * Math.PI
-//       r = Math.pow(2, 0.7* j) - 1 # this ensures there are lots of samples near the origin.
-//       x = r * Math.cos(theta) * props.dim
-//       y = r * Math.sin(theta) * props.dim
-//       lso = leastSquaresObjective({x,y}, props.pointClasses)
-//       return new THREE.Vector3(x, y, projectErrorForGraph lso)
+  buildGraphGeometry: function (props): THREE.ParametricGeometry {
+    var polarMeshFunction = function (i: number, j: number): THREE.Vector3 {
+      var theta = i * 2 * Math.PI
+      var r = Math.pow(2, 0.7* j) - 1 // this ensures there are lots of samples near the origin.
+      var x = r * Math.cos(theta) * props.dim
+      var y = r * Math.sin(theta) * props.dim
+      var lso = leastSquaresObjective({x,y}, props.pointClasses)
+      return new THREE.Vector3(x, y, projectErrorForGraph(lso))
+    }
 
-//     RESOLUTION = 24
-//     return new THREE.ParametricGeometry( polarMeshFunction, 8* RESOLUTION, 0.5* RESOLUTION, true )
+    var RESOLUTION = 24
+    return new THREE.ParametricGeometry( polarMeshFunction, 8* RESOLUTION, 0.5* RESOLUTION, true )
+  },
 
-//   colourGraphGeometry: (graphGeometry) ->
-//     graphGeometry.computeBoundingBox()
-//     zMin = graphGeometry.boundingBox.min.z
-//     zRange = graphGeometry.boundingBox.max.z - zMin
+  colourGraphGeometry: function (graphGeometry: THREE.ParametricGeometry): THREE.ParametricGeometry {
+    graphGeometry.computeBoundingBox()
+    var zMin = graphGeometry.boundingBox.min.z
+    var zRange = graphGeometry.boundingBox.max.z - zMin
 
-//     hue = 0.54
-//     sat = 0.8
-//     colourCurve = (z) -> 0.07 + 0.93*Math.pow(z, 2)
+    var hue = 0.54
+    var sat = 0.8
+    var colourCurve = (z) => 0.07 + 0.93*Math.pow(z, 2)
 
-//     faceIndices = [ 'a', 'b', 'c']
-//     for face in graphGeometry.faces
-//       totalZ = faceIndices
-//         .map (x) -> graphGeometry.vertices[face[x]].z
-//         .reduce ((a,b) -> a + b), 0
-//       normalizedZ = (totalZ - 3*zMin) / (3*zRange)
-//       face.color.setHSL( hue, sat, colourCurve(normalizedZ))
-//     return graphGeometry
+    for (var i=0; i < graphGeometry.faces.length; i++) {
+      var totalZ = graphGeometry.vertices.a.z + graphGeometry.vertices.b.z + graphGeometry.vertices.c.z
+      var normalizedZ = (totalZ - 3*zMin) / (3*zRange)
+      graphGeometry[i].color.setHSL( hue, sat, colourCurve(normalizedZ))
+    }
+    return graphGeometry
+  },
 
   mouseDown: function (e: React.SyntheticEvent): void {
     var intersections = this.raycast(this.camera, e).intersectObject(this.graph)
