089fa31ba52019109b782d5c2b463f4d4a392d00
Dan Fox <iamdanfox@gmail.com>
2015-02-17 20:43:28 +0000

    Clear Data / Reset Data works

--- scripts/MainPage.jsx
+++ scripts/MainPage.jsx
@@ -12,8 +12,10 @@ var {projectedError, projectedError2} = require("./LeastSquares.jsx");
 type F<U, V> = (x: U) => V;
 type P2 = {x: number;y: number}
 
-var points = require("../data/points.js");
-
+var MODES = {
+  TRY_HYPERPLANE: 1,
+  ADD_DATA: 2,
+};
 
 function project(arg: P2): number {
   var {x: x, y: y} = arg;
@@ -30,7 +32,9 @@ var MainPage = React.createClass({
   getInitialState: function(): {highlightedW: ?[number, number];cutoffs: [number, number]} {
     return {
       highlightedW: null,
-      cutoffs: [1, 1]
+      cutoffs: [1, 1],
+      mode: MODES.TRY_HYPERPLANE,
+      pointClasses: require("../data/points.js"),
     };
   },
 
@@ -70,30 +74,61 @@ var MainPage = React.createClass({
     }
   },
 
+  updateMode: function(nextMode: number): () => void {
+    return () =>
+      this.setState({
+        mode: nextMode,
+      });
+  },
+
+  handleClearData: function(): void {
+    this.setState({
+      pointClasses: this.state.pointClasses.map(function(){ return []; }),
+    });
+  },
+
+  handleResetData: function(): void {
+    this.setState({
+      pointClasses: require("../data/points.js"),
+    });
+  },
+
   render: function(): ?ReactElement {
-    var pointClasses = [points.class0, points.class1];
-    for (var i = 0; i < pointClasses.length; i = i + 1) {
-      pointClasses[i] = pointClasses[i].filter((p) => project(p) < this.state.cutoffs[i]);
+    var pointClasses = [];
+    for (var i = 0; i < this.state.pointClasses.length; i = i + 1) {
+      pointClasses[i] = this.state.pointClasses[i].filter((p) => project(p) < this.state.cutoffs[i]);
     }
 
     var style = {background: "#e0e0e0", width: this.props.dim, height: this.props.dim};
     return (
       <div className="main-page">
         <h2>Minimisation Objective: Squares of Misclassified points</h2>
+
+        <div>
+          <button disabled={this.state.mode === MODES.TRY_HYPERPLANE}
+            onClick={this.updateMode(MODES.TRY_HYPERPLANE)}>Try hyperplane</button>
+          <button disabled={this.state.mode === MODES.ADD_DATA}
+            onClick={this.updateMode(MODES.ADD_DATA)}>Add Data</button>
+          <button onClick={this.handleClearData}>Clear Data</button>
+          <button onClick={this.handleResetData}>Reset Data</button>
+        </div>
+
         <svg style={style} ref="svg" onMouseMove={this.mouseMove}>
           <g transform={"translate(" + this.props.dim / 2 + " " + this.props.dim / 2 + ") scale(1 -1)"}>
             <Axes dim={this.props.dim} />
             <AllPoints pointClasses={pointClasses} />
-            { this.makeHyperplane() }
+            { (this.state.mode === MODES.TRY_HYPERPLANE) && this.makeHyperplane() }
           </g>
         </svg>
 
         <Surface dim={this.props.dim} pointClasses={pointClasses} projectedError={projectedError}
           highlightedW={this.state.highlightedW} highlightW={this.highlightW} />
 
-        <DataSlider color="red" fullData={points.class0} project={project} dim={this.props.dim}
+        <DataSlider color="red" fullData={this.state.pointClasses[0]}
+          project={project} dim={this.props.dim}
           cutoff={this.state.cutoffs[0]} updateCutoff={this.updateCutoff(0)} />
-        <DataSlider color="blue" fullData={points.class1} project={project} dim={this.props.dim}
+        <DataSlider color="blue" fullData={this.state.pointClasses[1]}
+          project={project} dim={this.props.dim}
           cutoff={this.state.cutoffs[1]} updateCutoff={this.updateCutoff(1)} />
 
         <h2>Zero-One objective</h2>
