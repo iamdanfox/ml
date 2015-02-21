dbda5ee154abaf15e96e4be7aa653629d91121ba
Dan Fox <iamdanfox@gmail.com>
2015-02-21 18:21:57 +0000

    Place a ball inside ReactTHREE container
    makeGroups is essential it seems!

--- scripts/Surface2.jsx
+++ scripts/Surface2.jsx
@@ -2,19 +2,31 @@
 "use strict";
 
 var React = require("react");
-var {Scene, PerspectiveCamera} = require("react-three");
+var {Scene, PerspectiveCamera, Mesh, Object3D} = require("react-three");
+
 var THREE = require("three");
 
 
 
+var SPHERE_MATERIAL = new THREE.MeshBasicMaterial({color: 0xff0000});
+
+
 var Surface2 = React.createClass({
  render: function(): ?ReactElement {
+    var geometry = new THREE.SphereGeometry(3, 32, 32);
+    geometry.makeGroups();
+
     return (
       <Scene width={this.props.dim} height={this.props.dim} camera="maincamera">
 
         <PerspectiveCamera name="maincamera" fov={75} aspect={1} near={0.1} far={1000}
-          position={new THREE.Vector3(100, 100, 180)} lookat={new THREE.Vector3(0, 0, 0)}
-          up={new THREE.Vector3(0, 0, 1)} />
+          position={new THREE.Vector3(50, 50, 80)} lookat={new THREE.Vector3(0, 0, 0)} />
+
+
+        <Mesh
+          geometry={geometry}
+          material={SPHERE_MATERIAL}
+          position={new THREE.Vector3(0, 0, 0)} />
 
       </Scene>
     );
@@ -22,3 +34,4 @@ var Surface2 = React.createClass({
 });
 
 module.exports = Surface2;
+
