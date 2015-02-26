00ffd9a287af49c4c7b63ff04c22b96cbceace5d
Dan Fox <iamdanfox@gmail.com>
2015-02-26 17:33:09 +0000

    Compute the axis positions

--- scripts/ParallelCoords.jsx
+++ scripts/ParallelCoords.jsx
@@ -13,6 +13,18 @@ var ParallelCoords = React.createClass({
     initialCamera.position.z = 180;
 
     var initialScene = new THREE.Scene();
+    // add axes to scene
+    var RADIUS = 30;
+    var DIMENSION = 3;
+    var baseCoords = [0, 1, 2]
+      .map( (axis) => (axis / DIMENSION) * 2 * Math.PI )
+      .map(function(angle) {
+        return {
+          x: RADIUS * Math.cos(angle),
+          y: RADIUS * Math.sin(angle),
+        }
+      });
+    console.log(baseCoords);
 
     var initialRenderer = new THREE.WebGLRenderer({antialias: true});
     initialRenderer.setClearColor( 0x111111, 1 );
