743e0a805e58f2dc131b2fd9f29e40951e5636af
Dan Fox <iamdanfox@gmail.com>
2015-04-28 23:25:36 +0100

    First attempt: perceptron

--- scripts/MainPage.jsx
+++ scripts/MainPage.jsx
@@ -108,35 +108,80 @@ var MainPage = React.createClass({
         <HR />
 
         <div style={{width: "700px",
-        lineHeight: "35px",
+        lineHeight: "31px",
         display: "flex",
         flexDirection: "column",
         alignItems: "center"}}>
 
 
-          <p>Machine learning is all about learning from data in order to
-          make decisions. This article uses binary linear classifiers as an example.</p>
+          <p style={{width: '100%'}}>
+          A <em>binary classifier</em> is something that
+          can learn how to put unseen objects into one of two classes based on examples
+          that are already in those classes.
+          </p>
 
-          <HR />
+          <img src="http://i.imgur.com/BGPlM09.jpg" style={{maxWidth: "60%", margin: "1em" }} />
 
-          <p style={{width: '100%'}}>What is a binary linear classifier?</p>
+          <p>Linear classifiers accept objects represented as vectors, e.g. [10, -3, 0, 4],
+          and make their decisions using a linear function of the input vector. </p>
 
-          <p style={{width: '100%'}}>Well, a "binary classifier" is something that
-          takes in objects and puts then into one of two classes. These objects must be
-          represented as a mathematical vector, e.g. x1 = [1,2,3,4].</p>
-
-          <img src="http://i.imgur.com/BGPlM09.jpg" style={{maxWidth: "50%", margin: "1em" }} />
-
-          <p>A classifier is "linear" if it makes this decision using a
-          linear function of an input vector x.  If our objects are
-          one-dimensional, the decision boundary is just a point. In two
+          <p>
+          If our objects are
+          one-dimensional, the decision boundary is just a single number.
+          Everything on one side gets put in one class
+          and everything on the other side goes in the other. In two
           dimensions, it&apos;s a line and in three dimensions, the decision
-          boundary is a plane.</p>
-
+          boundary is a plane.
+          </p>
 
           <img src="http://i.imgur.com/jZg6nCx.jpg" style={{maxWidth: "120%", margin: "1em" }} />
 
+          <p>
+          Since the boundary is a hyperplane, we can represent it by a normal vector, n,
+          and an offset vector, c.  In practice, it&apos;s convenient
+          to add one fake dimension (with value 1) to every object in our data because
+          this lets us define the same hyperplane using just one vector.
+          This is called <em>homogeneous form</em>.</p>
 
+          <img src="http://i.imgur.com/XaHtzYx.jpg" style={{maxWidth: "50%", margin: "1em" }} />
+
+          <p>Our classifier can then use this vector n to decide the
+          class for an unseen vector x = [x1, x2].
+          It computes [x1, x2, 1] . n and if the result is positive,
+          returns class A, otherwise it returns class B.</p>
+
+          <p>The question is, how can we learn the value of the
+          vector n from our training examples?</p>
+
+          <HR />
+
+          <h2>The Perceptron</h2>
+
+          <p>A good idea would be to look through all our known training data
+          and find a hyperplane that classifies them all correctly.  The
+          Perceptron algorithm is one example of this.</p>
+
+          <code>
+          Initialize vector w<br />
+          cycle through points<br />
+          if w classifies a point x incorrectly, subtract x from w.<br />
+          repeat until w makes no mistakes
+          </code>
+
+          <p>[[[ANIMATION ALG VIS. AUto start when you scroll down.]]]</p>
+
+          <p>The perceptron is not perfect however.  Since it can choose any
+          hyperplane that makes no mistakes, it will sometimes give you one that
+          performs badly in the real world. </p>
+
+
+          <img src="http://i.imgur.com/QlNAio9.jpg" style={{maxWidth: "70%", margin: "1em" }} />
+
+          <p>Also, if there are no hyperplanes that separate the two classes,
+          the perceptron algorithm freaks out. (Note that Voted / Averaged Perceptrons
+          can help this.</p>
+
+          <HR />
 
         </div>
 
