0c565a334f1d52df1408bdbaffd8cb0604213597
Dan Fox <iamdanfox@gmail.com>
2015-05-02 21:34:45 +0100

    Shorter article.jsx

---
+++ scripts/Article.jsx
@@ -0,0 +1,52 @@
+/* @flow */
+"use strict";
+
+var React = require("react");
+var HR = require("./HR.jsx");
+
+var Header = require("./Header.jsx");
+var BinaryClassifierSection = require('./BinaryClassifierSection.jsx');
+var PerceptronSection = require('./PerceptronSection.jsx');
+var ObjectiveFunctionSection = require('./ObjectiveFunctionSection.jsx');
+var LogisticRegressionSection = require('./LogisticRegressionSection.jsx');
+var MaximumMarginSection = require('./MaximumMarginSection.jsx');
+
+
+var Article = React.createClass({
+  render: function(): ?ReactElement {
+    return (
+      <div className="main-page" style={{fontSize: "22px",
+        fontFamily: "Georgia, Cambria, 'Times New Roman'",
+        color: "rgba(0, 0, 0, 0.8)",
+        letterSpacing: "0.16px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"}}>
+
+        <Header />
+        <HR />
+
+        <div style={{width: "700px",
+        lineHeight: "31px",
+        paddingBottom: "10em"}}>
+
+          <BinaryClassifierSection />
+          <HR />
+
+          <PerceptronSection />
+          <HR />
+
+          <ObjectiveFunctionSection />
+          <HR />
+
+          <LogisticRegressionSection />
+          <HR />
+
+          <MaximumMarginSection />
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Article;
