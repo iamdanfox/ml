a1268b5921a196ad5756af407fb94962fd366a71
Dan Fox <iamdanfox@gmail.com>
2015-05-03 22:37:55 +0100

    Successful chopped MainVis out of SVM section

--- scripts/MaximumMarginSection.jsx
+++ scripts/MaximumMarginSection.jsx
@@ -1,10 +1,66 @@
 /* @flow */
 "use strict";
 
-var React = require("react");
-var MainVis = require("./MainVis.jsx");
-var MaximumMargin = require("./MaximumMargin.jsx");
+type P2 = {x: number; y: number};
+
+var CursorSphere = require('./CursorSphere.jsx');
+var DisplayWNumbers = require("./DisplayWNumbers.jsx");
+var Draggable3DScene = require("./Draggable3DScene.jsx");
+var HyperplaneVis = require("./HyperplaneVis.jsx");
 var K = require('./Katex.jsx');
+var MaximumMargin = require("./MaximumMargin.jsx");
+var Modes = require("./Modes.js");
+var ParametricGraph = require('./ParametricGraph.jsx');
+var React = require("react");
+var {ReplacePointsBar} = require("./ReplacePointsButton.jsx");
+
+
+
+var MaxMarginVis = React.createClass({
+  getInitialState: function(): {highlightedW: ?P2} {
+    return {
+      highlightedW: null,
+      pointClasses: require("../data/closePoints.js"),
+    };
+  },
+
+  highlightW: function(point: P2): void {
+    this.setState({highlightedW: point});
+  },
+
+  updatePointClasses: function(newPointClasses: [Array<P2>, Array<P2>]): void {
+    this.setState({pointClasses: newPointClasses});
+  },
+
+  render: function() {
+    var dim = 400;
+    return <div style={{width: "850px"}}>
+      <div style={{display: "flex", justifyContent: "space-between"}}>
+
+        <div style={{position: "relative"}}>
+          <HyperplaneVis dim={dim} mode={Modes.TRY_HYPERPLANE}
+            pointClasses={this.state.pointClasses} updatePointClasses={this.updatePointClasses}
+            highlightedW={this.state.highlightedW} highlightW={this.highlightW} />
+
+          <ReplacePointsBar callback={this.updatePointClasses}
+            style={{position: "absolute", bottom: 0, left: 0}} />
+        </div>
+
+        <Draggable3DScene dim={dim} pointClasses={this.state.pointClasses}
+            projectedError={MaximumMargin.objective} highlightW={this.highlightW}>
+          <ParametricGraph thetaResolution={120} rResolution={40} />
+          {this.state.highlightedW && <CursorSphere highlightedW={this.state.highlightedW} />}
+        </Draggable3DScene>
+      </div>
+
+      <div>
+        { this.state.highlightedW && <DisplayWNumbers w={this.state.highlightedW} />}
+      </div>
+    </div>;
+  }
+});
+
+
 
 var MaximumMarginSection = React.createClass({
   render: function(): ?ReactElement {
@@ -18,7 +74,6 @@ var MaximumMarginSection = React.createClass({
       <a href="https://www.cs.ox.ac.uk/teaching/materials13-14/machinelearning/lecture_lsvm.pdf">
       Slide 9</a></p>
 
-
       <p>
         <K tex="argmax_w \{ \frac{||w||^2}{2} \}" />
       </p>
@@ -26,9 +81,7 @@ var MaximumMarginSection = React.createClass({
         <K tex="s.t. ~~~~ y_i  ( w \cdot x_i ) \geq 1, ~~~~ \forall i" />
       </p>
 
-      <div style={{width: "850px"}}>
-        <MainVis dim={400} projectedError={MaximumMargin.objective}  />
-      </div>
+      <MaxMarginVis />
 
       <p>Describe lagrangian/KKT method for solving this (?!?! needs work to understand this)</p>
     </div>);
